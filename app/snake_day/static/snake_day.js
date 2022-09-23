53490061bb3fd22afaebbf26c6fa2de1212555fa
Alex Collino <arcollino@gmail.com>
2022-09-23 11:02:02 -0700

    clean up comments, improve file organization

--- app/snake_day/static/snake_day.js
+++ app/snake_day/static/snake_day.js
@@ -1,33 +1,9 @@
-//found at obj.agents array, arrayitem.full_name
-// const IMAGE_SOURCES = [
-//   // "iNaturalist",
-//   "Biopix Nature Photos",
-//   "Animal Diversity Web Traits and images",
-//   "CalPhotos",
-// ];
-
-// const FLICKR = "Flickr Group";
-
-//found at obj.rightsHolder
-// const FLICKR_SOURCES = [
-//   "Bernard DUPONT",
-//   "Patrick Randall",
-//   "Todd Pierson",
-//   "Reinaldo Aguilar",
-//   "Arthur Chapman",
-//   "Micheal Jewel",
-// ];
-
-// const INATURALIST_SOURCES = [
-//   "Kevin Metcalf",
-//   "Bridget Spencer",
-//   "mkosiewski",
-//   "Susan Elliott",
-//   "Mary Keim",
-//   "Marv Elliott",
-//   "Suzanne Cadwell",
-//   "Sarah Carline",
-// ];
+class SnakeError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "SnakeError";
+  }
+}
 
 // fetch the list of snake species from the animals API if needed,
 // otherwise return the list stored in localStorage
@@ -36,14 +12,15 @@ async function getSnakeArray() {
   if (snakeArray === null || JSON.parse(snakeArray).length === 0) {
     let resp = await fetch("/get/snakearray");
     let fullSnakeArray = await resp.json();
-    let namesOnlyArray = listSnakeNames(fullSnakeArray);
+    let namesOnlyArray = extractSnakeNames(fullSnakeArray);
     localStorage.setItem("snakeArray", JSON.stringify(namesOnlyArray));
     return namesOnlyArray;
   }
   return JSON.parse(snakeArray);
 }
 
-function listSnakeNames(snakeArray) {
+// Retrieve only the useful naming information from the snakes array
+function extractSnakeNames(snakeArray) {
   let namesList = [];
   snakeArray.forEach((snakeObj) => {
     if (verifyScientificName(snakeObj)) {
@@ -57,11 +34,16 @@ function listSnakeNames(snakeArray) {
   return namesList;
 }
 
+// Check if the snake object has a valid entry for a scientific name
+// ie. is in a form like 'Genus species', 'G. species'
 function verifyScientificName(snakeObj) {
   let scientificName = snakeObj.taxonomy.scientific_name;
   return scientificName != null && scientificName.includes(" ");
 }
 
+// Get the snake object's scientific name, piecing it together from
+// the taxonomy details if the given name is abbreviated ('G. species')
+// or if multiple names are given
 function parseScientificName(snakeObj) {
   let scientificName = snakeObj.taxonomy.scientific_name;
   let spaceSeparator = scientificName.indexOf(" ");
@@ -80,6 +62,7 @@ function parseScientificName(snakeObj) {
   return `${genus} ${species}`;
 }
 
+// If the daily snake needs to change, randomly select one from the stored array
 async function updateDailySnake() {
   if (checkIfOutdated()) {
     let snakeArray = await getSnakeArray();
@@ -94,40 +77,7 @@ async function updateDailySnake() {
   }
 }
 
-// check which indicies of the list haven't yet been used
-// function getOpenIndices(maxLength) {
-//   let openIndices = localStorage.getItem("openIndices");
-//   if (openIndices) {
-//     openIndices = JSON.parse(openIndices);
-//   }
-//   if (openIndices === null || openIndices.length === 0) {
-//     openIndices = Array.from({ length: maxLength }, (_, index) => index + 1);
-//   }
-//   return openIndices;
-// }
-
-// if there is not yet a daily snake, or if the daily snake is outdated,
-// get a not-yet-used snake from the snake array
-// async function getRandomSnake() {
-//   if (checkIfOutdated()) {
-//     let snakeArray = await getSnakeArray();
-//     if (snakeArray) {
-//       let openIndices = getOpenIndices(snakeArray.length);
-//       let randomValue = Math.floor(Math.random() * openIndices.length);
-//       let snakeIndex = openIndices.splice(randomValue, 1)[0];
-//       localStorage.setItem("openIndices", JSON.stringify(openIndices));
-//       let dailySnake = {
-//         snake: snakeArray[snakeIndex],
-//         date: new Date().toDateString(),
-//       };
-//       localStorage.setItem("dailySnake", JSON.stringify(dailySnake));
-//       verifyScientificName();
-//     }
-//   }
-//   return getStoredSnake();
-// }
-
-// check if the stored daily snake was not chosen today
+// Check if the stored daily snake has been removed or was not chosen today
 function checkIfOutdated() {
   let snakeObj = localStorage.getItem("dailySnake");
   let todayStr = new Date().toDateString();
@@ -143,7 +93,7 @@ function checkIfOutdated() {
   }
 }
 
-// get the daily snake stored in localStorage
+// Get the daily snake stored in localStorage
 function getStoredSnake() {
   let dailySnake = localStorage.getItem("dailySnake");
   if (dailySnake) {
@@ -152,40 +102,10 @@ function getStoredSnake() {
   return null;
 }
 
-// Some list entries have "Various" for scientific name or none at all
-// function verifyScientificName() {
-//   let storedSnake = getStoredSnake();
-//   if (storedSnake) {
-//     let scientificName = storedSnake.snake.taxonomy.scientific_name;
-//     if (scientificName == null || !scientificName.includes(" ")) {
-//       throw new SnakeError("Scientific name is not a valid species.");
-//     }
-//   }
-// }
-
-// Get the daily snake's scientific name, either from the scientific
-// name itself, or constructed using the genus if the given name's format
-// is 'C. viridis', for example
-// function parseScientificName(storedSnake) {
-// function parseScientificName() {
-//   let storedSnake = getStoredSnake();
-//   let scientificName = storedSnake.snake.taxonomy.scientific_name;
-//   let spaceSeparator = scientificName.indexOf(" ");
-//   let genus = scientificName.substring(0, spaceSeparator);
-//   if (scientificName.includes(".")) {
-//     genus = getStoredSnake().snake.taxonomy.genus;
-//   }
-//   let nameEnd = scientificName.indexOf(",");
-//   if (nameEnd === -1) {
-//     nameEnd = scientificName.indexOf(" and ");
-//     if (nameEnd < 0) {
-//       nameEnd = scientificName.length;
-//     }
-//   }
-//   let species = scientificName.substring(spaceSeparator + 1, nameEnd);
-//   return `${genus} ${species}`;
-// }
-
+// Search the iNaturalist API according to the daily snake's name, then
+// store the photo URLs. These are stored in case an image fails to load,
+// allowing the page to attempt loading the next image without another
+// request to the API.
 async function getINaturalistURLs() {
   let storedSnake = getStoredSnake();
   let iNatResp = await fetch(
@@ -210,21 +130,17 @@ async function getINaturalistURLs() {
   }
 }
 
-// results are ordered by a relevancy score, so first item is the best match
+// iNaturalist results are ordered by a relevancy score, so first item is the best match;
+// returns the usable photos from that search result.
 function parseINatResults(resultArray) {
   let bestResult = resultArray[0].record;
-  // let defaultImage = bestResult.default_photo;
-  // if (confirmPhotoUsable(defaultImage)) {
-  //   return defaultImage.url.replace("square.", "original.");
-  // }
-  // else {
-
-  // }
   return bestResult.taxon_photos.filter((photoObj) => {
     return confirmPhotoUsable(photoObj);
   });
 }
 
+// Checks if the photo has a creative commons license, is available to share, and
+// is not a gif
 function confirmPhotoUsable(photoObj) {
   let validLicense = photoObj.photo.license_code != null;
   let shareable = !photoObj.photo.attribution
@@ -234,108 +150,54 @@ function confirmPhotoUsable(photoObj) {
   return validLicense && shareable && staticImage;
 }
 
-// start from the first index of the photo list; if the default photo is
-// available, it will be the first item in the taxon photos, and it is also
+// Select a url, starting from the first index of the photo list; if the default photo
+// is available, it will be the first item in the taxon photos, and it is also
 // unlikely to feature a dead snake - which will rarely occur with snakes that
 // have relatively few photos (making a random photo selection more risky)
-function getINatURL() {
+function selectINatURL() {
   let dailySnake = getStoredSnake();
-  let photos = dailySnake.urls;
-  if (photos.length === 0) {
-    throw new SnakeError("The stored snake is out of URLs to try.");
-  } else {
-    // let randomIndex = Math.floor(Math.random() * photos.length);
-    // let targetPhoto = photos.splice(randomIndex, 1)[0].photo.original_url;
-    let targetPhoto = photos.shift().photo.original_url;
-    localStorage.setItem("dailySnake", JSON.stringify(dailySnake));
-    return targetPhoto;
+  let dailyURL = dailySnake.dailyURL;
+  if (!dailyURL) {
+    let photos = dailySnake.urls;
+    if (photos.length === 0) {
+      throw new SnakeError("The stored snake is out of URLs to try.");
+    } else {
+      let targetPhotoURL = photos.shift().photo.original_url;
+      dailySnake.dailyURL = targetPhotoURL;
+      localStorage.setItem("dailySnake", JSON.stringify(dailySnake));
+      return targetPhotoURL;
+    }
   }
+  return dailyURL;
 }
 
-// async function getEOLPageID(storedSnake) {
-// async function getEOLPageID() {
-//   let storedSnake = getStoredSnake();
-//   let scientificName = parseScientificName(storedSnake);
-//   let eolResp = await fetch(
-//     `https://eol.org/api/search/1.0.json?q=${scientificName}&page=1&exact=true`
-//   );
-//   let respData = await eolResp.json();
-//   let resultOptions = respData.results;
-//   if (respData.results.length > 1) {
-//     resultOptions = parseEOLSearchResults(respData.results, scientificName);
-//   }
-//   if (resultOptions.length === 0) {
-//     throw new SnakeError(
-//       `EOL search for ${scientificName} provided 0 usable results.`
-//     );
-//   }
-//   return resultOptions[0].id;
-// }
+// Add the daily snake image and title to the page
+function createSnakeDOM(title, url) {
+  snakeContainer = document.createElement("div");
+  snakeImage = document.createElement("img");
+  snakeImage.setAttribute("src", url);
+  snakeTitle = document.createElement("h1");
+  snakeTitle.textContent = title;
+  snakeContainer.append(snakeTitle);
+  snakeContainer.append(snakeImage);
+  document.body.append(snakeContainer);
+}
 
-// function parseEOLSearchResults(resultsArray, searchTerm) {
-//   return resultsArray.filter((resultObj) => {
-//     if (resultObj.title.search(/^\w+ \w+$/) === -1) {
-//       return false;
-//     } else {
-//       let contentArray = resultObj.content.split("; ");
-//       let regex = new RegExp("^" + searchTerm + " \\W*[A-Z]+.*[0-9]+.*$");
-//       let validContent = contentArray.filter((contentStr) => {
-//         return contentStr.search(regex) !== -1;
-//       });
-//       return validContent.length > 0;
-//     }
-//   });
-// }
-
-// async function getSnakeImageURLs(pageID, pageNum) {
-//   let eolResp = await fetch(
-//     `https://eol.org/api/pages/1.0/${pageID}.json?details=false&images_per_page=75&taxonomy=false&images_page=${pageNum}`
-//   );
-//   let respData = await eolResp.json();
-//   if (respData.taxonConcept.dataObjects != null) {
-//     let imageArray = respData.taxonConcept.dataObjects.filter((mediaObj) => {
-//       let validSource = mediaObj.agents.some((supplier) => {
-//         return IMAGE_SOURCES.includes(supplier.full_name);
-//       });
-//       let validPhotographer =
-//         FLICKR_SOURCES.includes(mediaObj.rightsHolder) ||
-//         INATURALIST_SOURCES.includes(mediaObj.rightsHolder);
-//       return validSource || validPhotographer;
-//     });
-//     if (imageArray.length === 0) {
-//       throw new SnakeImageError(
-//         `Snake page for EOL ID ${pageID}, page ${pageNum} had no valid images.`
-//       );
-//     }
-//     return imageArray;
-//   }
-//   throw new SnakeError(
-//     `Snake page on EOL ID ${pageID}, page ${pageNum} had no images at all.`
-//   );
-// }
-
-// async function getDailySnakeImage(pageCount) {
-//   await getRandomSnake();
-//   let pageID = await getEOLPageID();
-//   let images = await getSnakeImageURLs(pageID, pageCount);
-//   dailySnake = getStoredSnake();
-//   let title = `${dailySnake.snake.name}, ${parseScientificName(dailySnake)}`;
-//   let url = images[Math.floor(Math.random() * images.length)].eolMediaURL;
-//   createSnakeDOM(title, url);
-// }
-
-async function getDailySnakeImage() {
+// Select or retrieve the daily snake and load its image.
+async function loadDailySnakeImage() {
   await updateDailySnake();
   await getINaturalistURLs();
   let dailySnake = getStoredSnake().snake;
   let title = `${dailySnake.commonName}, ${dailySnake.scientificName}`;
-  let url = getINatURL();
+  let url = selectINatURL();
   createSnakeDOM(title, url);
 }
 
+// A limited recursive loop to retry loading the daily image, allowing
+// the page to load a different daily snake if something goes wrong.
 async function dailySnakeLoop(loopCount = 1) {
   try {
-    await getDailySnakeImage();
+    await loadDailySnakeImage();
   } catch (error) {
     console.error(error.message);
     if (loopCount < 10) {
@@ -353,31 +215,6 @@ async function dailySnakeLoop(loopCount = 1) {
   }
 }
 
-function createSnakeDOM(title, url) {
-  snakeContainer = document.createElement("div");
-  snakeImage = document.createElement("img");
-  snakeImage.setAttribute("src", url);
-  snakeTitle = document.createElement("h1");
-  snakeTitle.textContent = title;
-  snakeContainer.append(snakeTitle);
-  snakeContainer.append(snakeImage);
-  document.body.append(snakeContainer);
-}
-
 window.addEventListener("load", () => {
-  //getRandomSnake();
+  dailySnakeLoop();
 });
-
-class SnakeError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = "SnakeError";
-  }
-}
-
-// class SnakeImageError extends Error {
-//   constructor(message) {
-//     super(message);
-//     this.name = "SnakeImageError";
-//   }
-// }
