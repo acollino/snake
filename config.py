19a8aa28234460bc82eb91d2a391eb2c913002c1
Alex Collino <arcollino@gmail.com>
2022-09-28 10:31:21 -0700

    fix sqlalchemy db uri env

--- config.py
+++ config.py
@@ -7,7 +7,9 @@ class Config:
     """Set the base configuration for Flask."""
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    environ.get("DATABASE_URL", "").replace("postgres://", "postgresql://")
+    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL", "").replace(
+        "postgres://", "postgresql://"
+    )
     # replace because heroku uses 'postgres' - not supported by SQLAlchemy
     SECRET_KEY = environ.get("SECRET_KEY")
     PERMANENT_SESSION_LIFETIME = timedelta(days=1)
