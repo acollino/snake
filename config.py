9edf5b3291c3ac620fe4c971b913518f69a5dfc8
Alex Collino <arcollino@gmail.com>
2022-09-28 10:27:05 -0700

    add db url replacing line for accessing postgres

--- config.py
+++ config.py
@@ -7,7 +7,8 @@ class Config:
     """Set the base configuration for Flask."""
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
+    environ.get("DATABASE_URL", "").replace("postgres://", "postgresql://")
+    # replace because heroku uses 'postgres' - not supported by SQLAlchemy
     SECRET_KEY = environ.get("SECRET_KEY")
     PERMANENT_SESSION_LIFETIME = timedelta(days=1)
 
