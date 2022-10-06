0abaff138cd07567cb8448b94d20ef3f7fbb096a
Alex Collino <arcollino@gmail.com>
2022-10-06 15:45:08 -0700

    update seed file to inspect for table existence

--- seed.py
+++ seed.py
@@ -1,14 +1,23 @@
 # run with: python3 seed.py
 
 from dotenv import load_dotenv
+from sqlalchemy import inspect
 from app import init_app, db
 from app.models import AssociationMatchUser, Match, User
 
 load_dotenv()
 app = init_app("ProdConfig")
 app.app_context().push()
+inspector = inspect(db.engine)
 
-AssociationMatchUser.__table__.drop(db.engine)
-Match.__table__.drop(db.engine)
-User.__table__.drop(db.engine)
+# Perfomed because these tables should not typically be dropped,
+# and the foreign key dependencies require they be dropped in
+# this order, rather than using db.drop_all()
+if inspector.has_table(AssociationMatchUser.__table__):
+    AssociationMatchUser.__table__.drop(db.engine)
+if inspector.has_table(Match.__table__):
+    Match.__table__.drop(db.engine)
+if inspector.has_table(User.__table__):
+    User.__table__.drop(db.engine)
+
 db.create_all()
