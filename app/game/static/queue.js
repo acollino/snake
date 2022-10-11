39cc402e4d7f39db78b5700a2d37430dcb810d85
Alex Collino <arcollino@gmail.com>
2022-10-11 12:01:53 -0700

    add js file for queue to buffer player inputs

---
+++ app/game/static/queue.js
@@ -0,0 +1,45 @@
+class Queue {
+  constructor(data = null) {
+    this.length = 0;
+    if (data) {
+      this.enqueue(data);
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+  }
+
+  enqueue(data) {
+    let queueAddition = new Node(data);
+    if (this.tail) {
+      this.tail.next = queueAddition;
+      this.tail = queueAddition;
+    } else {
+      this.head = queueAddition;
+      this.tail = queueAddition;
+    }
+    this.length++;
+  }
+
+  dequeue() {
+    if (this.head === null) {
+      return null;
+    } else {
+      let queueRemoval = this.head;
+      this.head = queueRemoval.next;
+      if (this.head === null) {
+        this.tail = null;
+      }
+      queueRemoval.next = null;
+      this.length--;
+      return queueRemoval.data;
+    }
+  }
+}
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
